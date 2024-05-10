b637e70b88b280e241a06b9bdb66d1403df9e8e1
Duong Do Minh Chau <chau.duongdominh@employmenthero.com>
2024-05-10 17:54:44 +0700

    Implement endpoints

--- app.rb
+++ app.rb
@@ -6,11 +6,33 @@ require 'sinatra/json'
 
 # Main application
 class App < Sinatra::Base
+  set :show_exceptions, :after_handler
+
   before do
     content_type 'application/json'
   end
 
-  get '/' do
-    json({ message: 'Hello' })
+  error Sequel::ValidationFailed do
+    message = env['sinatra.error'].to_s
+    [422, json(message: message)]
+  end
+
+  get '/notes' do
+    json(Note.order(:id).all.map(&:values))
+  end
+
+  get '/notes/:id' do |id|
+    json(Note[id].values)
+  end
+
+  post '/notes' do
+    payload = JSON.parse(request.body.read)
+    note = Note.create(payload)
+    [201, json(note.values)]
+  end
+
+  delete '/notes/:id' do |id|
+    Note[id].destroy
+    [200, json(success: true)]
   end
 end
