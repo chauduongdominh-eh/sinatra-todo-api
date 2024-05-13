7c12838f3d9a596f1525fdb82a39e4b467817aa7
Duong Do Minh Chau <chau.duongdominh@employmenthero.com>
2024-05-13 12:07:31 +0700

    Implement naive authorization
    This is inappropriate, but it's easy to implement.
    I don't want to spend too much time on those things
    at this point

--- app.rb
+++ app.rb
@@ -4,17 +4,34 @@ require_relative './config/database'
 require 'sinatra'
 require 'sinatra/json'
 
+class UnauthorizedError < StandardError
+end
+
 # Main application
 class App < Sinatra::Base
   set :show_exceptions, :after_handler
 
   before do
     content_type 'application/json'
+    authorize!
+  end
+
+  def authorize!
+    username = request.env['HTTP_AUTHORIZATION']
+    @current_user = User.find(username: username)
+    raise UnauthorizedError unless @current_user
+  end
+
+  def error_response(status)
+    [status, json(message: env['sinatra.error'].to_s)]
   end
 
   error Sequel::ValidationFailed do
-    message = env['sinatra.error'].to_s
-    [422, json(message: message)]
+    error_response(422)
+  end
+
+  error UnauthorizedError do
+    error_response(401)
   end
 
   post '/users' do
@@ -32,10 +49,8 @@ class App < Sinatra::Base
   end
 
   post '/notes' do
-    username = request.env['HTTP_AUTHORIZATION']
     payload = JSON.parse(request.body.read)
-    current_user = User.find(username: username)
-    payload['user_id'] = current_user.id
+    payload['user_id'] = @current_user.id
     note = Note.create(payload)
     [201, json(note.values)]
   end
